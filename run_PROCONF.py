b8441d0f81374551bfded5f83ed47d4728fe796b
Paul GUÉNON / L3 intern avec Damien LAAGE 2020/03/26 <pguenon@frontale.debye.net>
2025-04-17 15:40:59 +0200

    "update"

--- run_PROCONF.py
+++ run_PROCONF.py
@@ -69,16 +69,13 @@ if step_to_perform=='all' or step_to_perform=='discretize_conformations' or step
     u_traj=open_trajectory(strucfile,trajfile)
 ##############################################################
 
-#################get terminal atoms###########################
+#################get terminal atoms and frames###########################
 if step_to_perform=='all' :
-    
+
     if os.path.exists(f'{output_dir}terminal_atoms.txt'):
         os.system(f'rm {output_dir}terminal_atoms.txt')
     terminal_atoms, RESIDS_SELECTED, RESNAMES_SELECTED,indices_aa=get_terminal_atoms_MDA(u_traj, dic)
     save_terminal_atoms(terminal_atoms, RESIDS_SELECTED, RESNAMES_SELECTED,output_dir)
-##############################################################
-
-################ get frames #####################################
     times, times_indices=filter_times_and_indices(u_traj, time_zero, delta_time,output_dir)
 #################################################################
 
