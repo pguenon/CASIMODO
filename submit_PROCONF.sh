88e4f93bf1e8ec848ff58e280917b4ca97a9d9f3
Paul GUÉNON / L3 intern avec Damien LAAGE 2020/03/26 <pguenon@frontale.debye.net>
2025-04-29 16:39:24 +0200

    update

--- submit_PROCONF.sh
+++ submit_PROCONF.sh
@@ -17,18 +17,18 @@ size_block=50000 # Size of the block to be analyzed in ps
 # These parameters are set to default values, but you can modify them if needed
 cutoff_distances=5 # Value in angstroms, to be considered as a contact, a distance should at least be once lower than this cutoff in the trajectory. 
 delta_residue=3 # The contact between two residues that are closer than this delta in the sequence are not considered because they are arleady super close in the sequence, example: the first contact to be considered for residue 1 is 1-4 if delta_residue=3 
-height_cutoff=1 # minimal difference of height, in percent of the maximum height, between a local minimum and a local maximum of a distribution to consider the local maximum as a peak
+height_cutoff=5 # minimal difference of height, in percent of the maximum height, between a local minimum and a local maximum of a distribution to consider the local maximum as a peak
 delta_time=1 # Time in ps between two frames to consider (if it is lower than the time step from the trajectory, then it takes instead the time step from the trajectory)
 number_of_states_to_show=10 # Number of states to show for each cluster, after the clustering
 ############################################################################################################
 
 ########################### OPTIONAL PARAMETERS #################################
 #optional parameters
-coordinates_to_add=(Data_files/stacking_ligand_cofactor.dat) # Path to time evolution of coordinates to add to the analysis
+coordinates_to_add=() # Path to time evolution of coordinates to add to the analysis
 # Example: coordinates_to_add=(Data_files/coordinate1.dat Data_files/coordinate2.dat)
-type_coordinates_to_add=(angle) # Type of coordinates to add to the analysis
+type_coordinates_to_add=() # Type of coordinates to add to the analysis
 # Example: type_coordinates_to_add=(type1 type2)
-barycenter_coordinates_to_add=(161_C4N) # Atom to consider as the barycenter of the coordinates to add
+barycenter_coordinates_to_add=() # Atom to consider as the barycenter of the coordinates to add
 # Example: barycenter_coordinates_to_add=(1_CA 2_CB)
 #################################################################################
 
